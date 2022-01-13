9475af19b80d994f27714f30164fae31912f0487
Samuel Rinnetmäki <samuel@iki.fi>
2022-01-13 14:05:32 +0200

    fixed install bugs

--- install.php
+++ install.php
@@ -2,21 +2,22 @@
 
  $create_config = true;
 
- $a = getopt("l:n:u:p");
- if (!isset($a) || count($a) > 4 ||
-     !isset($a['l']) || !isset($a['n']) ||
+ $a = getopt("l:n:u:p:t:");
+ if (!isset($a) || count($a) != 5 ||
+     !isset($a['l']) || !isset($a['n']) || !isset($a['t']) ||
      !isset($a['u']) || !isset($a['p'])) {
   echo "Usage:\n\n  php ".basename(__FILE__).
        " -l <db location> -n <db name> -u <db user> -p <db password>\n\n";
   die();
  }
- chdir(dirname(_FILE__));
+ chdir(dirname(__FILE__));
  if (file_exists('dbconfig.php')) {
   $msg = "Configuration file dbconfig.php exists. Overwrite?\n".
-         "y = Overwrite current configuration file. Creae new db tables.\n"
+         "y = Overwrite current configuration file. Create new db tables.\n".
 	 "n = Keep existing configuration file. Create new db tables.\n".
-	 "c = Cancel. Do nothing.";
-  $response = trim(readline(msg));
+	 "c = Cancel. Do nothing.\n\n";
+  echo $msg;
+  $response = trim(readline("Choice (y/n/c): "));
   switch ($response) {
    case 'c':
      die();
@@ -28,30 +29,30 @@
      echo "Overwriting dbconfig.php\n";
      break;
    default:
-     echo "Unknown response '$response'. Exiting.";
+     echo "Unknown response '$response'. Exiting.\n";
      die();
   }
  }
  if ($create_config) {
   $config_template = file_get_contents('dbconfig-sample.php');
   if (!$config_template) {
-   echo "Configuration file sample 'dbconfig-sample.php' not found. Exiting.";
+   echo "Configuration exsample 'dbconfig-sample.php' not found. Exiting.\n";
    die();
   }
-  str_replace('localhost', $a['l'], $config_template);
-  str_replace('mydatabase', $a['n'], $config_template);
-  str_replace('myusername', $a['u'], $config_template);
-  str_replace('mypassword', $a['p'], $config_template);
-  str_replace('timetrack', $a['t'], $config_template);
-  str_replace('mydatabase', $a['n'], $config_template);
-  file_put_contents('dbconfig.php', $config_template);
+  $search = array('localhost', 'mydatabase', 'myusername', 'mypassword', 'timetrack');
+  $replace = array($a['l'], $a['n'], $a['u'], $a['p'], $a['t']);
+  $config = str_replace($search, $replace, $config_template);
+  file_put_contents('dbconfig.php', $config);
  }
  require_once('dbconfig.php');
  error_reporting(E_ALL);
  $conn = mysqli_connect(DB_ADDR, DB_USER, DB_PASS);
+ if (!$conn) {
+  die("Failed to connect to '".DB_ADDR."' with username '".DB_USER."'!\n");
+ }
  $res = mysqli_select_db($conn, DB_DB);
  if (!$res) {
-  echo "Failed to connect to '".DB_ADDR."' with username '".DB_USER."'!";
+  echo "Failed to connect to '".DB_DB."' with username '".DB_USER."'!\n";
   die(mysqli_error($conn));
  }
 
@@ -71,13 +72,13 @@ CREATE TABLE IF NOT EXISTS ".DB_TABLE." (
 
 ";
 
- $res = mysqli_query($query);
+ $res = mysqli_query($conn, $query);
  @header('Content-Type: text/plain');
  if ($res) {
-  echo "Database table ".DB_DB.".times created succesfully!";
+  echo "Database table ".DB_DB.".times created succesfully!\n";
  }
  else {
-  echo "Could not create table ".DB_DB.".times: ".mysqli_error($conn);
+  echo "Could not create table ".DB_DB.".times: ".mysqli_error($conn)."\n";
  }
 
 ?>
